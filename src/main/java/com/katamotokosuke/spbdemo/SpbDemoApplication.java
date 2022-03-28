fec0cbd6263e36a4aadcfa09fb224feaeb292ae5
katamotokosuke <katamotokosuke0605@gmail.com>
2022-03-28 21:48:49 +0900

    first commit

---
+++ src/main/java/com/katamotokosuke/spbdemo/SpbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.katamotokosuke.spbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbDemoApplication.class, args);
+    }
+
+}
